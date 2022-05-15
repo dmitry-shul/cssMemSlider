a8d0577e91b807a8bccb3d4fb5ce07a1b5356ea3
Dmitry Shulzhytski <matemV@mail.ru>
2022-05-15 21:35:44 +0300

    fix: remove comments (Fri, May 6, 2022 06:00:00 PM)

--- cssMemSlider/script.js
+++ cssMemSlider/script.js
@@ -29,15 +29,11 @@ window.addEventListener("resize", () => {
 
 //изменение картинкаи
 function changeSlide(index) {
-  //index = index < 0 ? 0 : index;
-  //index = index > 3 ? 3 : index;
   image1.style = `margin-top: -${clientWidth > 480 ? 400 * index : 280 * index}px;`;
 }
 
 //изменение текста
 function changeText(index) {
-  //index = index < 0 ? 0 : index;
-  //index = index > 3 ? 3 : index;
   text.style = `transform: translateY(${index > i ? "-" : ""}10px); opacity: 0;`;
   setTimeout(() => {
     text.innerText = "";
@@ -50,13 +46,3 @@ function changeText(index) {
     i = index;
   }, 300);
 }
-
-//пролистывание с помощью колеса мыши
-/*document.addEventListener('wheel', (event) => {
-  event = event || window.event;
-  let y = event.deltaY || event.detail || event.wheelDelta;
-  console.log(y);
-  let k = i;
-  changeSlide(y > 0 ? ++k : k--);
-  changeText(y > 0 ? ++k : k--);
-});*/
